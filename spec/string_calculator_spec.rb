6b2619bcbd3d064d2adaf339159532bed05152ab
Deepak Pandey <ykcir.deep@hotmail.com>
2025-04-22 14:25:22 +0530

    spec when only number is provided

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -5,5 +5,9 @@ RSpec.describe StringCalculator do
     it 'returns 0 for an empty string' do
       expect(StringCalculator.add("")).to eq(0)
     end
+
+    it 'returns the number itself when only one number is provided' do
+      expect(StringCalculator.add("5")).to eq(5)
+    end
   end
 end
