9babfa6409e6970db6def5dfde129ea5631867f2
Deepak Pandey <ykcir.deep@hotmail.com>
2025-04-22 14:34:54 +0530

    method chaged to handle delimiter and numbers

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,7 +1,24 @@
 module StringCalculator
   def self.add(numbers)
-    numbers = numbers.split(/[\s,]+/)
-    numbers = numbers.map!(&:to_i)
-    numbers.sum
+    delimiter, nums = parse_delimiter(numbers)
+    numbers = nums.split(/#{delimiter}/)
+
+    numbers.sum do |num|
+      n = num.to_i
+      raise "negative numbers not allowed: #{n}" if n < 0
+      n <= 1000 ? n : 0
+    end
+  end
+
+  private
+
+  def self.parse_delimiter(input)
+    if input.start_with?('//')
+      delimiter, nums = input.split("\n", 2)
+      delimiter = delimiter.start_with?('//[') ? Regexp.escape(delimiter[3...-1]) : Regexp.escape(delimiter[2..])
+      [delimiter, nums]
+    else
+      [/[\s,]+/, input]
+    end
   end
 end
